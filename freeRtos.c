7bfddd0a2719188f1782b429d1bd9e1fd65c3e0b
mahadeep <121691397+mahadeep5@users.noreply.github.com>
2023-05-21 22:19:38 +0530

    Add files via upload

---
+++ freeRtos.c
@@ -0,0 +1,28 @@
+#define redLed 7
+#define greenLed  8
+void setup(){
+    PinMode(greenLed, OUTPUT);
+    xTaskCreate(redLedTask, "red Led Task,128,NULL,1,NULL");
+    xTaskCreate(greenLedTask, "green Led Task,128,NULL,1,NULL");
+
+}
+void loop(){
+    void redLedTask(void *pvParameter){
+        PinMode(redLed, OUTPUT);
+        while(1){
+            digitalWrite(redLed, HIGH);
+            delay(500);
+            digitalWrite(redLed, LOW);
+            delay(500);
+        }
+    }
+     void greenLedTask(void *pvParameter){
+        PinMode(greenLed, OUTPUT);
+        while(1){
+            digitalWrite(greenLed, HIGH);
+            delay(500);
+            digitalWrite(greenLed, LOW);
+            delay(500);
+        }
+    }
+}
